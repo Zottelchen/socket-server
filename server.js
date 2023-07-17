f0e6cf716ddca54a93f9b18cdfa5c347e2f69a45
Zottelchen <github@zottelchen.com>
2023-07-17 18:21:37 +0200

    🎨 Improve structure/format of code

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const User = require("./models/user");
 const PORT = process.env.PORT || 5000;
+const chalk = require("chalk");
 
 // Connect to DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/node-testing",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, res) => {
-    if (err) {
-      console.error("ERROR: Error connecting to the database. " + err);
-    } else {
-      console.info("INFO: Connected to Database!");
-      /*
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/node-testing", { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+	if (err) {
+		console.error(`ERROR: Error connecting to the database. ${err}`);
+	} else {
+		console.info("INFO: Connected to Database!");
+		/*
       console.log('INFO: Wiping all socketUuids...');
       User.find({}, (_err, _res) => {
         _res.forEach(user => {
@@ -27,9 +25,8 @@ mongoose.connect(
       });
       console.log('INFO: Wiped socketUuids');
       */
-    }
-  }
-);
+	}
+});
 
 // Create HTTP server
 const app = express();
@@ -40,7 +37,13 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Setup logging
-app.use(morgan("combined"));
+app.use(
+	morgan(
+		`🍄M ➡ ${chalk.gray("[:date[clf]]")} "${chalk.green(":method")} ${chalk.blue(":url")} HTTP/:http-version" ${chalk.gray(":remote-addr")}   ${chalk.yellow(
+			":status",
+		)} :res[content-length] ":referrer" ${chalk.gray('":user-agent"')}`,
+	),
+);
 console.info("Morgan logging enabled.");
 
 // Setup static files
@@ -48,7 +51,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP root
 app.get("/", function (req, res) {
-  res.render("index");
+	res.render("index");
 });
 
 const update = require("./routes/update");
@@ -59,43 +62,43 @@ app.use("/stats", stats);
 
 // socket.io server
 const socketConfig = require("./socket-config");
-let io = require("socket.io")(server);
+const io = require("socket.io")(server);
 socketConfig(io);
 
 // Error handling
 /// Handle 500
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).render("error", {
-    pageTitle: "Error 500 - Server goofed.",
-    errorMessage: "Something broke :(",
-    statusCode: "500",
-    textColor: "#FFD800",
-  });
+	console.error(err.stack);
+	res.status(500).render("error", {
+		pageTitle: "Error 500 - Server goofed.",
+		errorMessage: "Something broke :(",
+		statusCode: "500",
+		textColor: "#FFD800",
+	});
 });
 app.get("/500", function (req, res) {
-  res.status(500).render("error", {
-    pageTitle: "Error 500 - Server goofed.",
-    errorMessage: "Something broke :(",
-    statusCode: "500",
-    textColor: "#FFD800",
-  });
+	res.status(500).render("error", {
+		pageTitle: "Error 500 - Server goofed.",
+		errorMessage: "Something broke :(",
+		statusCode: "500",
+		textColor: "#FFD800",
+	});
 });
 /// Handle 404
 app.use((req, res) => {
-  res.status(404).render("error", {
-    pageTitle: "Error 404 - Page Not Found",
-    errorMessage: "Page not found. :/",
-    statusCode: "404",
-    textColor: "#FF6A3D",
-  });
+	res.status(404).render("error", {
+		pageTitle: "Error 404 - Page Not Found",
+		errorMessage: "Page not found. :/",
+		statusCode: "404",
+		textColor: "#FF6A3D",
+	});
 });
 
 // Start server
 server.start = () => {
-  server.listen(PORT, () => {
-    console.info(`INFO: Server started on port ${PORT}.`);
-  });
+	server.listen(PORT, () => {
+		console.info(`INFO: Server started on port ${PORT}.`);
+	});
 };
 
 module.exports = server;
--- socket-config.js
+++ socket-config.js
@@ -2,178 +2,177 @@ const User = require("./models/user");
 const Message = require("./models/message");
 
 module.exports = (io) => {
-  const sendCachedMessage = (user) => {
-    Message.findOne({ user: user._id }).exec((err, message) => {
-      if (err) {
-        console.log("ERROR: Could not search for message.");
-      } else {
-        if (!message) {
-          console.log("INFO: No messages belonging to user " + user.macAddress);
-          return null;
-        } else {
-          io.to(user.socketUuid).emit("msg qos1", message.message);
-          console.log(
-            "INFO: Cached message found and delivered for user " +
-              user.macAddress
-          );
-        }
-      }
-    });
-  };
+	const sendCachedMessage = (user) => {
+		Message.findOne({ user: user._id }).exec((err, message) => {
+			if (err) {
+				console.log("ERROR: Could not search for message.");
+			} else {
+				if (!message) {
+					console.log(`INFO: No messages belonging to user ${user.macAddress}`);
+					return null;
+				} else {
+					io.to(user.socketUuid).emit("msg qos1", message.message);
+					console.log(
+						`INFO: Cached message found and delivered for user ${user.macAddress}`,
+					);
+				}
+			}
+		});
+	};
 
-  const cacheMessage = (user, message) => {
-    let newMessage = Message({
-      user: user._id,
-      message: message,
-    });
-    newMessage.save((err, data) => {
-      if (err) {
-        console.log("ERROR: Could not save message.");
-        console.log(err);
-      } else {
-        console.log("INFO: Saved message for " + user.macAddress);
-      }
-    });
-  };
+	const cacheMessage = (user, message) => {
+		const newMessage = Message({
+			user: user._id,
+			message: message,
+		});
+		newMessage.save((err, data) => {
+			if (err) {
+				console.log("ERROR: Could not save message.");
+				console.log(err);
+			} else {
+				console.log(`INFO: Saved message for ${user.macAddress}`);
+			}
+		});
+	};
 
-  io.on("connect", (socket) => {
-    console.log(`INFO: New client with id ${socket.id} connected.`);
+	io.on("connect", (socket) => {
+		console.log(`INFO: New client with id ${socket.id} connected.`);
 
-    // Ask client for mac address
-    socket.emit("send mac", "");
+		// Ask client for mac address
+		socket.emit("send mac", "");
 
-    socket.on("hey", (data) => {
-      console.log("INFO: Received hey from client.");
-      socket.send("OK");
-    });
+		socket.on("hey", (data) => {
+			console.log("INFO: Received hey from client.");
+			socket.send("OK");
+		});
 
-    // When user sends their mac address, update the socket id
-    socket.on("mac", (data) => {
-      console.log(data);
-      console.log(`INFO: Received mac ${data.macAddress} from ${socket.id}`);
+		// When user sends their mac address, update the socket id
+		socket.on("mac", (data) => {
+			console.log(data);
+			console.log(`INFO: Received mac ${data.macAddress} from ${socket.id}`);
 
-      // Search for user, update their socketUuid.
-      User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
-        if (err) {
-          console.log("ERROR: Could not search for user.");
-        } else {
-          if (!user) {
-            console.log(
-              `INFO: User ${data.macAddress} not found, creating new user.`
-            );
-            let newUser = User({
-              macAddress: data.macAddress,
-              socketUuid: socket.id,
-            });
-            newUser.save((_err, _data) => {
-              if (_err) {
-                console.log("ERROR: Could not save user.");
-              } else {
-                console.log(
-                  `INFO: Created new user with mac address ${_data.macAddress}`
-                );
-              }
-            });
-          } else {
-            user.socketUuid = socket.id;
-            user.save((_err) => {
-              console.log(`INFO: Updated socket id of user ${user.macAddress}`);
-              sendCachedMessage(user);
-            });
-          }
-        }
-      });
-    });
+			// Search for user, update their socketUuid.
+			User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
+				if (err) {
+					console.log("ERROR: Could not search for user.");
+				} else {
+					if (!user) {
+						console.log(
+							`INFO: User ${data.macAddress} not found, creating new user.`,
+						);
+						const newUser = User({
+							macAddress: data.macAddress,
+							socketUuid: socket.id,
+						});
+						newUser.save((_err, _data) => {
+							if (_err) {
+								console.log("ERROR: Could not save user.");
+							} else {
+								console.log(
+									`INFO: Created new user with mac address ${_data.macAddress}`,
+								);
+							}
+						});
+					} else {
+						user.socketUuid = socket.id;
+						user.save((_err) => {
+							console.log(`INFO: Updated socket id of user ${user.macAddress}`);
+							sendCachedMessage(user);
+						});
+					}
+				}
+			});
+		});
 
-    // When user sends a message, send it to the destination
-    socket.on("msg", (data, id) => {
-      // Forward message to pair
-      console.log(`INFO: Got message addressed to ${data.macAddress}`);
-      User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
-        if (err) {
-          console.log("ERROR: Could not search.");
-        } else if (user) {
-          if (user.socketUuid !== null) {
-            // Send message
-            io.to(user.socketUuid).emit("msg", data);
-            console.log("INFO: Delivered message to " + user.socketUuid);
-          } else {
-            console.log(`INFO: User ${user.macAddress} is disconnected.`);
-            socket.emit("partner offline");
-          }
-        } else {
-          console.log(
-            `INFO: User ${data.macAddress} could not be found in the database.`
-          );
-          socket.emit("unknown user");
-        }
-      });
-    });
+		// When user sends a message, send it to the destination
+		socket.on("msg", (data, id) => {
+			// Forward message to pair
+			console.log(`INFO: Got message addressed to ${data.macAddress}`);
+			User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
+				if (err) {
+					console.log("ERROR: Could not search.");
+				} else if (user) {
+					if (user.socketUuid !== null) {
+						// Send message
+						io.to(user.socketUuid).emit("msg", data);
+						console.log(`INFO: Delivered message to ${user.socketUuid}`);
+					} else {
+						console.log(`INFO: User ${user.macAddress} is disconnected.`);
+						socket.emit("partner offline");
+					}
+				} else {
+					console.log(
+						`INFO: User ${data.macAddress} could not be found in the database.`,
+					);
+					socket.emit("unknown user");
+				}
+			});
+		});
 
-    socket.on("msg qos1", (data, id) => {
-      // Forward message to pair
-      console.log(`INFO: Got message addressed to ${data.macAddress}`);
-      User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
-        if (err) {
-          console.log("ERROR: Could not search.");
-        } else if (user) {
-          if (user.socketUuid !== null) {
-            // Send message
-            io.to(user.socketUuid).emit("msg qos1", data);
-            console.log("INFO: Delivered message to " + user.socketUuid);
-          } else {
-            console.log(
-              `INFO: User ${user.macAddress} is disconnected, message will be cached.`
-            );
-            socket.emit("partner offline");
-            cacheMessage(user, data);
-          }
-        } else {
-          console.log(
-            `INFO: User ${data.macAddress} could not be found in the database.`
-          );
-          socket.emit("unknown user");
-        }
-      });
-    });
+		socket.on("msg qos1", (data, id) => {
+			// Forward message to pair
+			console.log(`INFO: Got message addressed to ${data.macAddress}`);
+			User.findOne({ macAddress: data.macAddress }).exec((err, user) => {
+				if (err) {
+					console.log("ERROR: Could not search.");
+				} else if (user) {
+					if (user.socketUuid !== null) {
+						// Send message
+						io.to(user.socketUuid).emit("msg qos1", data);
+						console.log(`INFO: Delivered message to ${user.socketUuid}`);
+					} else {
+						console.log(
+							`INFO: User ${user.macAddress} is disconnected, message will be cached.`,
+						);
+						socket.emit("partner offline");
+						cacheMessage(user, data);
+					}
+				} else {
+					console.log(
+						`INFO: User ${data.macAddress} could not be found in the database.`,
+					);
+					socket.emit("unknown user");
+				}
+			});
+		});
 
-    socket.on("get stats", (data, id) => {
-      console.log("INFO: user stats requested");
-      let stats = {
-        users: 0,
-        usersOnline: 0,
-      };
-      User.countDocuments({ socketUuid: null }).exec((err, usersOnline) => {
-        if (err) {
-          console.log("ERROR: Could not count.");
-        } else {
-          stats.usersOnline = usersOnline;
-        }
-        User.countDocuments({}).exec((err, users) => {
-          if (err) {
-            console.log("ERROR: Could not count.");
-          } else {
-            stats.users = users;
-            socket.emit("stats", stats);
-          }
-        });
-      });
-    });
+		socket.on("get stats", (data, id) => {
+			console.log("INFO: user stats requested");
+			const stats = {
+				users: 0,
+				usersOnline: 0,
+			};
+			User.countDocuments({ socketUuid: null }).exec((err, usersOnline) => {
+				if (err) {
+					console.log("ERROR: Could not count.");
+				} else {
+					stats.usersOnline = usersOnline;
+				}
+				User.countDocuments({}).exec((err, users) => {
+					if (err) {
+						console.log("ERROR: Could not count.");
+					} else {
+						stats.users = users;
+						socket.emit("stats", stats);
+					}
+				});
+			});
+		});
 
-    socket.on("disconnect", (reason) => {
-      console.log(`INFO: Client with id ${socket.id} disconnected.`);
-      User.findOne({ socketUuid: socket.id }).exec((err, user) => {
-        if (err) {
-          console.log("ERROR: Could not search.");
-        } else if (user) {
-          user.socketUuid = null;
-          user.save((_err, data) => {
-            console.log(`INFO: Erased socket id of ${user.macAddress}.`);
-          });
-        } else {
-          console.log("WARN: Could not find user after disconnecting.");
-        }
-      });
-    });
-  });
+		socket.on("disconnect", (reason) => {
+			console.log(`INFO: Client with id ${socket.id} disconnected.`);
+			User.findOne({ socketUuid: socket.id }).exec((err, user) => {
+				if (err) {
+					console.log("ERROR: Could not search.");
+				} else if (user) {
+					user.socketUuid = null;
+					user.save((_err, data) => {
+						console.log(`INFO: Erased socket id of ${user.macAddress}.`);
+					});
+				} else {
+					console.log("WARN: Could not find user after disconnecting.");
+				}
+			});
+		});
+	});
 };
